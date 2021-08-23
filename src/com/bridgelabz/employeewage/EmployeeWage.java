65a9e4da60821936f6c80b8d3580e7460602398a
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-23 22:47:53 +0530

    RANDOM checking

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,5 +3,11 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employeeWage system;");
+
+        double Checkout = Math.floor(Math.random() * 10) % 2;
+        if (Checkout == 1)
+            System.out.println("Employee Present.");
+        else
+            System.out.println("Employee absent.");
     }
 }
