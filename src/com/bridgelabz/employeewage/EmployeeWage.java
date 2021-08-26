9c022cd631c73f62bd584781d111e47ef5617282
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-26 16:12:39 +0530

    added Part time Employee.

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,20 +2,32 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
     public static final int WAGE_PER_HOUR = 20;
-    public static final int FULL_DAY_HOUR = 8;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employeeWage system;");
 
-        double Checkout = Math.floor(Math.random() * 10) % 2;
-        if (Checkout == 1) {
-            System.out.println("Employee Present.");
-            int dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
-            System.out.println("Daily employee Wage::" +dailyWage);
+        int partTime = 1;
+        int fullTIme = 2;
+        int empHours = 0;
+
+        double Checkout = Math.floor(Math.random() * 10) % 3;
+        if (Checkout == fullTIme) {
+            System.out.println("Full-time Employee.");
+            empHours = FULL_DAY_HOURS;
         }
-        else {
-            System.out.println("Employee absent.");
+        else if (Checkout == partTime){
+            System.out.println("Part-time Employee.");
+            empHours = PART_TIME_HOURS;
+        } else {
+            System.out.println("Employee Absent.");
+
+            empHours = 0;
         }
+
+        int dailyWage = empHours * WAGE_PER_HOUR;
+        System.out.println("Daily employee Wage::" +dailyWage);
 
     }
 }
