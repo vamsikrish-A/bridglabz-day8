27e32ed52ed92acfe8621a0dbeca27defa405048
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-26 17:02:23 +0530

    Switch cases

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,13 +1,28 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAYS_HOUR = 2;
+    public static final int PART_DAYS_HOUR = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to employeeWage system;");
 
-        double Checkout = Math.floor(Math.random() * 10) % 2;
-        if (Checkout == 1)
-            System.out.println("Employee Present.");
-        else
-            System.out.println("Employee absent.");
+        int empHours = 0;
+
+        int checkOut = (int) Math.floor(Math.random() * 10) % 3;
+        switch (checkOut) {
+            case FULL_DAYS_HOUR:
+                empHours = 8;
+                break;
+            case PART_DAYS_HOUR:
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
+        }
+
+        int dailyWage = empHours * WAGE_PER_HOUR;
+        System.out.println("Employee Wage::"+dailyWage);
     }
 }
