d167551a80a4179cfb774ea5842a3d4dc007da80
vamsikrish-A <krishnavamsi165@gmail.com>
2021-08-26 18:00:08 +0530

    MAx Hrs & days

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,13 +5,15 @@ public class EmployeeWage {
     public static final int FULL_DAYS_HOUR = 2;
     public static final int PART_DAYS_HOUR = 1;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employeeWage system;");
 
-        int empHours = 0, empWagePerMonth = 0;
+        int empHours = 0, empWagePerMonth = 0, totalHrs = 0, totalWorkingDays = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        while (totalHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int checkOut = (int) Math.floor(Math.random() * 10) % 3;
             switch (checkOut) {
                 case FULL_DAYS_HOUR:
@@ -23,10 +25,12 @@ public class EmployeeWage {
                 default:
                     empHours = 0;
             }
-            int dailyWage = empHours * WAGE_PER_HOUR;
-            empWagePerMonth += dailyWage;
-            System.out.println("Employee Wage "+day+" is ::"+dailyWage);
+            totalHrs += empHours;
+            System.out.println("Day#:"+totalWorkingDays + " Emp hr:"+empHours);
+
         }
-        System.out.println("Employee per month::"+empWagePerMonth);
+        System.out.println("Employee total Hours ::"+totalHrs);
+        int totalEmpWage = totalHrs * WAGE_PER_HOUR;
+        System.out.println("Employee per month::"+totalEmpWage);
     }
 }
